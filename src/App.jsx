e91114c6b0b69d7b4901e09a7796242fdb095584
Reymond Julio <reymondjulio58@gmail.com>
2023-07-18 17:00:02 +0700

    implement dom  friends with react and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gradient-to-r from-blue-300 to-blue-500">
+      <h1 className="font-sans text-center text-3xl font-bold text-slate-200 drop-shadow-md py-4">Friend's Book</h1>
+      {/* Container Card */}
+      <div className="container mx-auto w-full p-8 flex flex-wrap justify-center gap-6">
+        {/* Reymond Julio */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+          <div className="">
+            <img className="w-60 h-60 rounded  overflow-hidden" src="/public/reymond.jpg" alt="reymond" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">Reymond Julio</h3>
+            <p className="text-center text-lg text-slate-800">26 years old</p>
+            <p className="text-center text-lg text-slate-500">5 mutual friends</p>
+            <button className="bg-green-900 text-white p-2 rounded w-full self-center hover:bg-green-800">Request Accepted</button>
+          </div>
+        </div>
+
+        {/* M Haidar Hanif */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+          <div>
+            <img className="w-60 h-60 rounded overflow-hidden" src="/public/haidar.jpg" alt="haidar" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">M Haidar Hanif</h3>
+            <p className="text-center text-lg text-slate-800">30 years old</p>
+            <p className="text-center text-lg text-slate-500">10 mutual friends</p>
+            <button className="bg-blue-500 text-white p-2 rounded w-full self-center hover:bg-blue-400">Confirm</button>
+            <button className="bg-slate-300 rounded text-black font-semibold p-2 w-full self-center hover:bg-slate-400">Delete</button>
+          </div>
+        </div>
+
+        {/* Christopher Kevin */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ">
+          <div>
+            <img className="w-60 h-60 rounded overflow-hidden" src="/public/christopher.jpg" alt="christopher" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">Christopher Kevin</h3>
+            <p className="text-center text-lg text-slate-800">28 years old</p>
+            <p className="text-center text-lg text-slate-500">3 mutual friends</p>
+            <button className="bg-green-900 text-white p-2 rounded w-full self-center hover:bg-green-800">Request Accepted</button>
+          </div>
+        </div>
+
+        {/* Caroline */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+          <div>
+            <img className="w-60 h-60 rounded overflow-hidden" src="/public/caroline.jpg" alt="caroline" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">Caroline Elizabeth </h3>
+            <p className="text-center text-lg text-slate-800">24 years old</p>
+            <p className="text-center text-lg text-slate-500">8 mutual friends</p>
+            <button className="bg-green-900 text-white p-2 rounded w-full self-center hover:bg-green-800">Request Accepted</button>
+          </div>
+        </div>
+
+        {/* Victoria */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+          <div>
+            <img className="w-60 h-60 rounded overflow-hidden" src="/public/victoria.jpg" alt="vicky" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">Victoria Grace</h3>
+            <p className="text-center text-lg text-slate-800">29 years old</p>
+            <p className="text-center text-lg text-slate-500">2 mutual friends</p>
+            <button className="bg-blue-500 text-white p-2 rounded w-full self-center hover:bg-blue-400">Confirm</button>
+            <button className="bg-slate-300 rounded text-black font-semibold p-2 w-full self-center hover:bg-slate-400">Delete</button>
+          </div>
+        </div>
+
+        {/* Daniel */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ">
+          <div>
+            <img className="w-60 h-60 rounded overflow-hidden" src="/public/daniel.jpg" alt="daniel" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">Daniel William</h3>
+            <p className="text-center text-lg text-slate-800">27 years old</p>
+            <p className="text-center text-lg text-slate-500">7 mutual friends</p>
+            <button className="bg-green-900 text-white p-2 rounded w-full self-center hover:bg-green-800">Request Accepted</button>
+          </div>
+        </div>
+
+        {/* Jessica */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ">
+          <div>
+            <img className="w-60 h-60 rounded overflow-hidden" src="/public/jessica.jpg" alt="jessica" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">Jessica Lynn</h3>
+            <p className="text-center text-lg text-slate-800">26 years old</p>
+            <p className="text-center text-lg text-slate-500">12 mutual friends</p>
+            <button className="bg-blue-500 text-white p-2 rounded w-full self-center hover:bg-blue-400">Confirm</button>
+            <button className="bg-slate-300 rounded text-black font-semibold p-2 w-full self-center hover:bg-slate-400">Delete</button>
+          </div>
+        </div>
+
+        {/* Kelly */}
+        <div className="max-w-fit h-fit mt-6 rounded shadow-lg shadow-blue-300 delay-75 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+          <div>
+            <img className="w-60 h-60 rounded overflow-hidden" src="/public/kelly.jpg" alt="kelly" />
+          </div>
+          <div className="flex flex-col justify-center items-center p-6  space-y-2 bg-slate-200">
+            <h3 className="text-center text-xl font-semibold">Kelly Marie</h3>
+            <p className="text-center text-lg text-slate-800">23 years old</p>
+            <p className="text-center text-lg text-slate-500">10 mutual friends</p>
+            <button className="bg-blue-500 text-white p-2 rounded w-full self-center hover:bg-blue-400">Confirm</button>
+            <button className="bg-slate-300 rounded text-black font-semibold p-2 w-full self-center hover:bg-slate-400">Delete</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
