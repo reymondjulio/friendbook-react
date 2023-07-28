174d77e157d562b29a3f8358d843986d63e8c82d
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-28 16:59:58 +0700

    feat: button props destructure

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,16 +1,16 @@
 /* eslint-disable react/prop-types */
-export function Button(props) {
+export function Button({ variant, children, ...props }) {
   return (
     <button
       className={`p-2 rounded w-full self-center ${
-        (props.variant === "primary" &&
+        (variant === "primary" &&
           `bg-blue-500 text-white  hover:bg-blue-400`) ||
-        (props.variant === "secondary" &&
+        (variant === "secondary" &&
           `bg-slate-300 text-black hover:bg-slate-200`)
       }`}
       {...props}
     >
-      {props.children}
+      {children}
     </button>
   );
 }
