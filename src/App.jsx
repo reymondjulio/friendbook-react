6512f5990d3c4d823eb5f90bbadd78e5ea084cef
M Haidar Hanif <git@mhaidarhanif.com>
2023-07-28 15:43:18 +0700

    refactor: friend card with prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { PropTypes } from "prop-types";
 
 import { Button } from "./components/button";
 import { dataFriends } from "./data/friends";
 
-function App() {
+export default function App() {
   const [friends, setFriends] = useState(dataFriends);
 
   function confirmFriend(id) {
@@ -25,7 +26,9 @@ function App() {
   return (
     <main className="bg-slate-100 ">
       <header className="bg-white">
-        <h1 className="text-center text-3xl font-bold py-4 text-blue-500 shadow-md">Friendbook</h1>
+        <h1 className="text-center text-3xl font-bold py-4 text-blue-500 shadow-md">
+          Friendbook
+        </h1>
       </header>
 
       {/* <pre>{JSON.stringify(friends, null, 2)}</pre> */}
@@ -34,25 +37,8 @@ function App() {
       <ul className="container mx-auto w-full p-8 grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 justify-center items-center gap-12">
         {friends.map((friend) => {
           return (
-            <li key={friend.id} className="max-w-fit h-fit justify-self-center self-start rounded bg-white transition ease-in-out delay-150">
-              <img className="w-60 h-60 rounded object-cover  overflow-hidden" src={friend.avatarURL} alt={friend.name} />
-
-              <div className="flex flex-col justify-center items-center p-6 space-y-2">
-                <h3 className="text-center text-xl font-semibold">{friend.name}</h3>
-                <p className="text-center text-lg text-slate-800">{friend.age} years old</p>
-                <p className="text-center text-lg text-slate-500">{friend.mutualFriends} mutual friends</p>
-              </div>
-
-              {!friend.isConfirmed && (
-                <div className="p-2 space-y-2">
-                  <Button variant="primary" onClick={() => confirmFriend(friend.id)}>
-                    Confirm
-                  </Button>
-                  <Button variant="secondary" onClick={() => deleteFriend(friend.id)}>
-                    Delete
-                  </Button>
-                </div>
-              )}
+            <li key={friend.id} className="self-start">
+              <FriendCard friend={friend} />
             </li>
           );
         })}
@@ -61,4 +47,46 @@ function App() {
   );
 }
 
-export default App;
+function FriendCard({ friend }) {
+  return (
+    <div className="max-w-fit h-fit rounded bg-white transition ease-in-out delay-150">
+      <img
+        className="w-60 h-60 rounded object-cover  overflow-hidden"
+        src={friend.avatarURL}
+        alt={friend.name}
+      />
+
+      <div className="flex flex-col justify-center items-center p-6 space-y-2">
+        <h3 className="text-center text-xl font-semibold">{friend.name}</h3>
+        <p className="text-center text-lg text-slate-800">
+          {friend.age} years old
+        </p>
+        <p className="text-center text-lg text-slate-500">
+          {friend.mutualFriends} mutual friends
+        </p>
+      </div>
+
+      {!friend.isConfirmed && (
+        <div className="p-2 space-y-2">
+          <Button variant="primary" onClick={() => confirmFriend(friend.id)}>
+            Confirm
+          </Button>
+          <Button variant="secondary" onClick={() => deleteFriend(friend.id)}>
+            Delete
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+FriendCard.propTypes = {
+  friend: {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    avatarURL: PropTypes.string.isRequired,
+    age: PropTypes.number.isRequired,
+    mutualFriends: PropTypes.number.isRequired,
+    isConfirmed: PropTypes.boolean.isRequired,
+  },
+};
