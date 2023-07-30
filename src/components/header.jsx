8806ac0010ff1beb913e1592662573f18db9c6bc
Reymond Julio <reymondjulio58@gmail.com>
2023-07-30 13:09:27 +0700

    improve header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,12 @@
 export function Header() {
   return (
     <header className="bg-blue-500">
-      <h1 className="text-center text-3xl font-bold py-4 text-white ">Friendbook</h1>
+      <div className="container mx-auto bg-blue-500 p-4 flex justify-between">
+        <h1 className="text-left text-2xl font-bold text-white">Friendbook</h1>
+        <div>
+          <input type="text" className="px-8 py-1 rounded-lg bg-gray-200 focus:outline-none" placeholder="Search Friends..." />
+        </div>
+      </div>
     </header>
   );
 }
