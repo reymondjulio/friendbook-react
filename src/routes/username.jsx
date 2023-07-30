c9a27ddc8c1e283405ccb3cfca1f0fcdc83f6c0a
Reymond Julio <reymondjulio58@gmail.com>
2023-07-30 13:10:15 +0700

    styling button profile page

--- src/routes/username.jsx
+++ src/routes/username.jsx
@@ -4,6 +4,10 @@ import dataFriends from "../data/friends.json";
 
 import { Header } from "../components/header";
 
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+
+import { UserPlusIcon } from "@heroicons/react/24/solid";
+
 export function loader({ params }) {
   const friend = dataFriends.find((friend) => {
     if (friend.username === params.username) {
@@ -21,10 +25,10 @@ export default function Username() {
       <Header />
       <main className="container mx-auto w-full h-auto">
         <div className="container mx-auto max-w-4xl ">
-          <img className="w-full h-96 mb-4 bg-cover rounded-lg" src={friend.coverURL} alt={friend.name} />
+          <img className="w-full h-96 mb-4 bg-cover bg-center rounded-md" src={friend.coverURL} alt={friend.name} />
         </div>
 
-        <div className="container max-w-4xl mx-auto flex justify-between bg-white p-4">
+        <div className="container max-w-4xl mx-auto flex justify-between bg-white border-2 border-white p-4">
           <img className="w-32 h-32 rounded-full bg-cover" src={friend.avatarURL} alt={friend.name} />
 
           <div className="mr-auto pl-4 space-y-2">
@@ -32,8 +36,16 @@ export default function Username() {
             <p className="text-gray-500 font-bold">{friend.mutualFriends} Mutual Friends</p>
           </div>
 
-          <div></div>
+          <div className="flex gap-x-2 h-8 self-center">
+            <button className="flex items-center gap-x-1 bg-slate-200 hover:bg-slate-300 px-4 py-2 rounded-md">
+              <UserPlusIcon className="w-4 h-4 " /> Friend
+            </button>
+            <button className="flex items-center gap-x-1 rounded-md bg-blue-500 hover:bg-blue-400 text-white px-4 py-2 text-sm">
+              <ChatBubbleLeftRightIcon className="w-4 h-4" /> Message
+            </button>
+          </div>
         </div>
+
         {/* <h1>{friend.name}</h1>
 
         <pre>{JSON.stringify(friend, null, 2)}</pre> */}
