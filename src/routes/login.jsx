3e7c626c6fd380f6019141de64a09d2e566e98af
Reymond Julio <reymondjulio58@gmail.com>
2023-07-31 15:52:22 +0700

    create the login page layout and colour the content text

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -2,9 +2,21 @@ import { Header } from "../components/header";
 
 export default function Login() {
   return (
-    <div>
+    <div className="bg-slate-100">
       <Header />
-      <h1 className="bg-yellow-500">Hello World</h1>
+
+      <div className="container mx-auto w-full max-w-6xl flex justify-between items-center h-screen">
+        <div className="max-w-lg w-full space-y-4 mb-44">
+          <h1 className="text-6xl text-blue-500 font-bold">Friendbook</h1>
+          <p className="text-3xl font-semibold">Connect with friends and the world around you on Friendbook.</p>
+        </div>
+
+        <div>
+          <form action="">
+            <input type="text" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
