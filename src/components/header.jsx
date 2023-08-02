b0e6c32b11f3e21c18572b9c34f51717764deafa
Reymond Julio <reymondjulio58@gmail.com>
2023-08-02 15:24:22 +0700

    improve the layout of header components for mobile phone screens

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="bg-blue-500 sticky top-0 z-10">
-      <div className="container mx-auto bg-blue-500 p-4 flex justify-between items-center ">
-        <h1 className="text-left text-2xl font-bold text-white">
+    <header className="w-full  bg-blue-500 sticky top-0 z-10">
+      <div className="container mx-auto max-6-xl w-full bg-blue-500 p-4 flex flex-wrap gap-y-6 justify-center gap-x-12 sm:justify-between items-center ">
+        <h1 className="text-left text-2xl font-bold text-white ">
           <Link to="/"> Friendbook</Link>
         </h1>
 
-        <ul className="flex gap-x-16">
+        <ul className="flex gap-x-4 lg:gap-x-16 self-center">
           <li>
             <Link to="/register" className="text-white font-semibold">
               Register
@@ -21,8 +21,8 @@ export function Header() {
           </li>
         </ul>
 
-        <div>
-          <input type="text" className="px-8 py-1 rounded-lg bg-gray-200 focus:outline-none" placeholder="Search Friends..." />
+        <div className="flex justify-center">
+          <input type="text" className=" px-8 py-1 rounded-lg text-sm bg-gray-200 focus:outline-none w-3/4 sm:w-full" placeholder="Search Friends..." />
         </div>
       </div>
     </header>
