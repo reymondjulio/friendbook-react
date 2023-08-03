f2c0ff6d484939a69878937f1a67897a1c5e0278
Reymond Julio <reymondjulio58@gmail.com>
2023-08-03 13:39:54 +0700

    change the font size in the button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,12 +2,7 @@
 export function Button({ variant, children, ...props }) {
   return (
     <button
-      className={`p-2 rounded w-full self-center ${
-        (variant === "primary" &&
-          `bg-blue-500 text-white  hover:bg-blue-400`) ||
-        (variant === "secondary" &&
-          `bg-slate-300 text-black hover:bg-slate-200`)
-      }`}
+      className={`p-2 rounded w-full self-center ${(variant === "primary" && `bg-blue-500 text-white text-sm sm:text-md hover:bg-blue-400`) || (variant === "secondary" && `bg-slate-300 text-black text-sm sm:text-md hover:bg-slate-200`)}`}
       {...props}
     >
       {children}
